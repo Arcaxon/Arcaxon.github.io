544b47e56eeae08a3576f6c4fb359ff0f7740abf
Arcaxon <Arcaxon@users.noreply.github.com>
2024-11-08 07:17:56 -0330

    webfishing update

---
+++ assets/js/webFishing.js
@@ -0,0 +1,20 @@
+
+// WEBFISHING JAVASCRIPT FUN TIMES!
+
+
+//FISHING CATCHING MINIGAME NAME OF IDs
+
+
+//The Modal where you CATCH the FEESH!
+//catchFishBox - SPECIFICALLY FOR IF the fish is ALIVE or not
+//catchFish - The actual image
+//catchBox - The BOX with the Text about the fish
+//catchQuality
+//catchSize
+//catchSizeCM
+//catchDescription
+
+
+//The Lil Notification Thingie
+//fishingNotice
+//fishingExclamation
